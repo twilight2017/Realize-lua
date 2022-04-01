e773ce43f39caf90781bf4934b7abb04f7f28b19
twilight2017 <liuzixuanp@163.com>
2022-04-01 19:49:35 +0800

    ch06 second commit

--- ch06/inst_operators.go
+++ ch06/inst_operators.go
@@ -18,3 +18,127 @@ func _unaryArith(i Instruction, vm LuaVM, op ArithOp) {
 	vm.Arith(op)
 	vm.Replace(a)
 }
+
+//实现全部算术指令
+func add(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPADD)
+} //+
+
+//-
+func sub(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPSUB)
+}
+
+//*
+func mul(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPMUL)
+}
+
+//%
+func mod(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPMOD)
+}
+
+//^
+func pow(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPPOW)
+}
+
+// /
+func div(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPDIV)
+}
+
+// //
+func idiv(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPIDIV)
+}
+
+// &
+func band(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPBAND)
+}
+
+// |
+func bor(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPBOR)
+}
+
+// -
+func bxor(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPBXOR)
+}
+
+// <<
+func shl(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPSHL)
+}
+
+// >>
+func shr(i Instruction, vm LuaVM) {
+	_binaryArith(i, vm, LUA_OPSHR)
+}
+
+// -
+func unm(i Instruction, vm LuaVM) {
+	_unaryArith(i, vm, LUA_UNM)
+}
+
+//-
+func bnot(i Instruction, vm LuaVM) {
+	_unaryArith(i, vm, LUA_BNOT)
+}
+
+//长度计算函数
+func _len(i Instruction, vm LuaVM) {
+	a, b, _ = i.ABC()
+	a += 1
+	b += 1
+
+	vm.Len(b)
+	vm.Replace(a)
+}
+
+//concat():将连续n个寄存器（起止索引分别由操作数B和操作数C指定）的值进行拼接
+//将计算结果放入另一个寄存器（索引由操作数A决定）
+func concat(i Instruction, vm LuaVM) {
+	a, b, c = i.ABC()
+	a += 1
+	b += 1
+	c += 1
+	n := c - b + 1   //获取n的长度
+	vm.CheckStack(n) //检查栈中剩余空间是否有n个
+	for i := b; i <= c; i++ {
+		vm.PushValue(i)
+	}
+	vm.Contact(n)
+	vm.Repalce(a)
+}
+
+//比较指令：比较寄存器或常量表里的两个值（索引分别由操作数B和C指定）
+//如何比较结果和操作数A（转换为布尔值）匹配，则跳过下一条指令
+func _compare(i Instruction, vm LuaVM, op CompareOp) {
+	a, b, c = i.ABC()
+
+	vm.GetRK(b)
+	vm.GetRK(c) //将值推入栈顶
+	if vm.Compare(-2, -1, op) != (a != 0) {
+		vm.AddPC(1)
+	}
+	vm.Pop(2) //清理栈顶
+}
+
+//==
+func eq(i Instruction, vm LuaVM) {
+	_compare(i, vm, LUA_OPEQ)
+}
+
+//<
+func lt(i Instruction, vm LuaVM) {
+	_compare(i, vm, LUA_OPLT)
+}
+
+//<=
+func le(i, Instruction, vm LuaVM) {
+	_compare(i, vm, LUA_OPLE)
+}
